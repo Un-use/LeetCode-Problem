5c6376e974cea56f913f3fed8a11c40f801b402d
Unuse <360838842@qq.com>
2020-01-06 17:11:07 +0800

    P1234

--- src/com/unuse/p1201_p1250/P1234_BalancedString.java
+++ src/com/unuse/p1201_p1250/P1234_BalancedString.java
@@ -1,8 +1,5 @@
 package com.unuse.p1201_p1250;
 
-import java.util.HashMap;
-import java.util.Map;
-
 /**
  * @author Unuse
  * @date 2019/12/25
@@ -32,20 +29,92 @@ public class P1234_BalancedString {
 		}
 		int singleCount = s.length() / 4;
 		int minLength = 0;
-		for (int i : arr) {
+		int[] tempArr = new int[4];
+		for (int i = 0; i < 4 ; i++) {
 			if (arr[i] > singleCount) {
 				arr[i] -= singleCount;
-				minLength += arr[i];
 			} else {
 				arr[i] = 0;
 			}
+			minLength += arr[i];
+			tempArr[i] = arr[i];
+		}
+		if (minLength == 0) {
+			return 0;
 		}
 
+		int result = s.length();
+		int left = 0, right = 0;
+		boolean needBreak = false;
+		while (right < s.length()) {
+			if (arr[0] == 0) {
+				tempArr[0] = 0;
+			}
+			if (arr[1] == 0) {
+				tempArr[1] = 0;
+			}
+			if (arr[2] == 0) {
+				tempArr[2] = 0;
+			}
+			if (arr[3] == 0) {
+				tempArr[3] = 0;
+			}
+			while (tempArr[0] > 0 || tempArr[1] > 0 || tempArr[2] > 0 || tempArr[3] > 0) {
+				if (right == s.length()) {
+					needBreak = true;
+					break;
+				}
+				switch (s.charAt(right++)) {
+					case 'Q':
+						tempArr[0]--;
+						break;
+					case 'W':
+						tempArr[1]--;
+						break;
+					case 'E':
+						tempArr[2]--;
+						break;
+					case 'R':
+						tempArr[3]--;
+						break;
+					default:
+				}
+			}
+			if (needBreak) {
+				break;
+			}
 
+			if (right - left < result) {
+				result = right - left;
+			}
+
+			int index;
+			while (true) {
+				c = s.charAt(left++);
+				if ('Q' == c) {
+					index = 0;
+				} else if ('W' == c) {
+					index = 1;
+				} else if ('E' == c) {
+					index = 2;
+				} else {
+					index = 3;
+				}
+				if (arr[index] > 0 && ++tempArr[index] > 0) {
+					break;
+				}
+			}
+
+			if (right - left + 1 < result) {
+				result = right - left + 1;
+			}
+		}
+
+		return result;
 	}
 
 	public static void main(String[] args) {
-		System.out.println(balancedString("WWEQERQWQWWRWWERQWEQ"));
+		System.out.println(balancedString("QQQQ"));
 	}
 
 }
